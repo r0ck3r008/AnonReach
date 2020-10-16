0be1b3d7af6fd2197a890fbce1aa97c21804a9f3
r0ck3r008 <naman.arora@tutanota.com>
2020-10-16 00:11:46 -0400

    Changed type of the map that nbrtbl has
    	The nbrtbl node maps the nbrnodes by having a hash map of a
    	slice of pointers to nbrnodes with byte of the character in
    	node's hash as key.

--- router/nbrmap.go
+++ router/nbrmap.go
@@ -1,9 +1,15 @@
 package router
 
+import (
+	"fmt"
+	utils "github.com/r0ck3r008/AnonReach/utils"
+)
+
 type nbrnode struct {
 }
 
 type nbrmap struct {
 	hash string
-	arr  map[byte]*nbrnode
+	arr  map[byte][]*nbrnode
+}
 }
