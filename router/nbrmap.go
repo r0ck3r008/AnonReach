04655c85224fc41708c9a59aa2f761220cc8432a
r0ck3r008 <naman.arora@tutanota.com>
2020-10-16 00:14:11 -0400

    Added nbrtbl functions for insert and find

--- router/nbrmap.go
+++ router/nbrmap.go
@@ -13,4 +13,29 @@ type nbrmap struct {
 	hash string
 	arr  map[byte][]*nbrnode
 }
+
+func (nmap_p *nbrmap) add_nbr(hash *string) {
+	var lvl int = utils.Getlvl(&(nmap_p.hash), hash)
+	var indx byte = nmap_p.hash[lvl]
+
+	if node_slice, err := nmap_p.arr[indx]; !err {
+		if len(node_slice) >= utils.MAXDEPTH {
+			return
+		}
+	} else {
+		nmap_p.arr[indx] = make([]*nbrnode, utils.MAXDEPTH)
+	}
+	hash_str := *hash
+	node := &nbrnode{hash_str}
+	nmap_p.arr[indx] = append(nmap_p.arr[indx], node)
+}
+
+func (nmap_p *nbrmap) get_nbr(hash *string) ([]*nbrnode, error) {
+	var lvl int = utils.Getlvl(&(nmap_p.hash), hash)
+	var indx byte = nmap_p.hash[lvl]
+	if node_slice, err := nmap_p.arr[indx]; !err {
+		return node_slice, nil
+	} else {
+		return nil, fmt.Errorf("No Neighbour Found!")
+	}
 }
