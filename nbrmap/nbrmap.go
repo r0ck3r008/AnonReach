ed06e16b094de467f57f01debb39193beed46c6e
r0ck3r008 <naman.arora@tutanota.com>
2020-10-19 20:26:58 -0400

    Moved NbrMap to seprate module

--- nbrmap/nbrmap.go
+++ nbrmap/nbrmap.go
@@ -1,20 +1,23 @@
-package router
+package nbrmap
 
-import (
-	"fmt"
-	utils "github.com/r0ck3r008/AnonReach/utils"
-)
+import "fmt"
+import utils "github.com/r0ck3r008/AnonReach/utils"
 
 type nbrnode struct {
 	hash string
 }
 
-type nbrmap struct {
+type NbrMap struct {
 	hash string
 	arr  map[byte][]*nbrnode
 }
 
-func (nmap_p *nbrmap) add_nbr(hash *string) {
+func (nmap_p *NbrMap) NbrMapInit(hash *string) {
+	nmap_p.hash = *hash
+	nmap_p.arr = make(map[byte][]*nbrnode)
+}
+
+func (nmap_p *NbrMap) AddNbr(hash *string) {
 	var lvl int = utils.Getlvl(&(nmap_p.hash), hash)
 	var indx byte = nmap_p.hash[lvl]
 
@@ -30,7 +33,7 @@ func (nmap_p *nbrmap) add_nbr(hash *string) {
 	nmap_p.arr[indx] = append(nmap_p.arr[indx], node)
 }
 
-func (nmap_p *nbrmap) get_nbr(hash *string) ([]*nbrnode, error) {
+func (nmap_p *NbrMap) GetNbr(hash *string) ([]*nbrnode, error) {
 	var lvl int = utils.Getlvl(&(nmap_p.hash), hash)
 	var indx byte = nmap_p.hash[lvl]
 	if node_slice, err := nmap_p.arr[indx]; !err {
