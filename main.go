7033e9db52e9345b21046d22998cdd86a535b0d6
r0ck3r008 <naman.arora@tutanota.com>
2020-10-19 20:28:59 -0400

    Basic initialization to check compile

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 // Package main is responsible for providing the interface to the application.
 package main
 
-import objstore "github.com/r0ck3r008/AnonReach/objstore"
 import router "github.com/r0ck3r008/AnonReach/router"
 
 func main() {
-	/* TODO */
+	var route_p *router.Router
+	hash := "hash"
+	route_p.RouterInit(&hash, 12345)
 }
