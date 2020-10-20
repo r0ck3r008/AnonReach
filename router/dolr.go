d73f2fad7c0392ab66b2cb5a3b245c6d1658461a
r0ck3r008 <naman.arora@tutanota.com>
2020-10-20 01:44:55 -0400

    DOLR API in separate source file

---
+++ router/dolr.go
@@ -0,0 +1,17 @@
+package router
+
+func (route_p *Router) Publish() {
+
+}
+
+func (route_p *Router) Unpublish() {
+
+}
+
+func (route_p *Router) RouteToObj() {
+
+}
+
+func (route_p *Router) RouteToNode() {
+
+}
